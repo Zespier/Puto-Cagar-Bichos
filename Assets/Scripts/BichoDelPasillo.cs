336f44552d732930b3dd1f6eaefbef771e68d0c7
Zespier <pabloreco.redondo@gmail.com>
2026-01-31 03:44:29 +0100

    Preparando la animacion de la puerta cuando te mata el del pasillo

--- Assets/Scripts/BichoDelPasillo.cs
+++ Assets/Scripts/BichoDelPasillo.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class BichoDelPasillo : MonoBehaviour {
@@ -6,11 +7,25 @@ public class BichoDelPasillo : MonoBehaviour {
     public float hideSpeedd = 2f;
     public Transform attackPosition;
     public Transform hidePosition;
+    public Transform pomoDeLaPuerta;
+    public Transform pomoOrientation;
+    public Transform puerta;
+    public Transform puertaOrientation;
+    public float timeToOpenTheDoor = 3;
+    public float doorKillingAnimationTime = 0.2f;
+
+    private Vector3 _defaultForward;
+    private float _openDoorTimer;
+    private bool _killing;
+    private Vector3 _puertaDefaultUp;
 
     public static BichoDelPasillo instance;
 
     private void Awake() {
         if (!instance) { instance = this; }
+
+        _defaultForward = pomoDeLaPuerta.transform.forward;
+        _puertaDefaultUp = puerta.transform.up;
     }
 
     private void Update() {
@@ -21,5 +36,40 @@ public class BichoDelPasillo : MonoBehaviour {
         } else {
             transform.position = Vector3.MoveTowards(transform.position, attackPosition.position, Time.deltaTime * approachSpeedd);
         }
+
+        if (transform.position == attackPosition.position) {
+            _openDoorTimer += Time.deltaTime;
+
+        } else {
+            _openDoorTimer = 0;
+        }
+
+        pomoDeLaPuerta.forward = Vector3.Lerp(_defaultForward, pomoOrientation.forward, _openDoorTimer / timeToOpenTheDoor);
+
+        if (_openDoorTimer >= timeToOpenTheDoor) {
+            DoorAnimationANDKILL();
+        }
+    }
+
+    public void DoorAnimationANDKILL() {
+        if (!_killing) {
+            StartCoroutine(C_DoorAnimationANDKILL());
+        }
+    }
+
+    private IEnumerator C_DoorAnimationANDKILL() {
+        _killing = true;
+
+        float timer = Time.time;
+
+        while (Time.time - timer < doorKillingAnimationTime) {
+
+            puerta.up = Vector3.Lerp(_puertaDefaultUp, puertaOrientation.up, (Time.time - timer) / doorKillingAnimationTime);
+            yield return null;
+        }
+
+        puerta.up = puertaOrientation.up;
+
+        //MATAR
     }
 }
